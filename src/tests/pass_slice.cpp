5555910de8c541e83860d9dbd55233d463f31c5c
ianzapolsky <ianzapolsky@gmail.com>
2015-04-05 13:47:48 -0400

    changed range function to slice function, access list by integer bounds. merged with refactor

--- src/tests/pass_slice.cpp
+++ src/tests/pass_slice.cpp
@@ -8,7 +8,7 @@
 int main() {
     auto ints = Collection<int>(std::vector<int> {1, 2, 3, 4, 5});
 
-    auto ints2 = ints.range(2,4);
+    auto ints2 = ints.slice(1,3);
     assert(ints2[0] == 2);
     assert(ints2[1] == 3);
     assert(ints2[2] == 4);
